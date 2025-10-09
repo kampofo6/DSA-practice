a17b41531640371e5c586ae7feff9423848b2588
Kojo <kampofo6@gmail.com>
2025-10-09 18:04:13 -0400

    implemented basic array structure

---
+++ implementations/arrays/Array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Array {
+
+    public static void main(String args[]){
+
+        // pretty basic array implementation
+
+        int[] nums = new int[]{1,2,3,4,5};
+
+        System.out.println(Arrays.toString(nums));
+    }
+    
+}
