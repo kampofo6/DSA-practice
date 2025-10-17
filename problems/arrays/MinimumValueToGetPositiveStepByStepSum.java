4d3d2719ede8891c96c75723478ab8be5ebd02ba
Kojo <kampofo6@gmail.com>
2025-10-16 23:05:08 -0400

    My solution & intuition for leetcode 1413

---
+++ problems/arrays/MinimumValueToGetPositiveStepByStepSum.java
@@ -0,0 +1,36 @@
+/* initialize sum & lowestNum, summate nums[i] (prefix sum), 
+use Math.min to compare sum & lowestNum, if lowestNum > 0, return 1, else return 1 - lowestNum.*/
+
+public class MinimumValueToGetPositiveStepByStepSum {
+    
+
+    public int minStartvalue( int[] nums){
+
+        int sum = 0;
+        int lowestNum = 0;
+        int startVal = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            sum += nums[i];
+
+            lowestNum = Math.min(lowestNum, sum);
+
+            if(lowestNum > 0){
+                return 1;
+            }
+
+            else{
+
+                startVal = 1 - lowestNum;
+            }
+        }
+
+        return startVal;
+
+
+
+
+    }
+
+}
