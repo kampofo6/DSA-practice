a5987ff2b647ef3185f799f723afb20dcafff125
Kojo <kampofo6@gmail.com>
2026-01-01 23:11:30 -0500

    Write concise intuiton for leetcode 1493

--- problems/arrays/LongestSubarrayOnesAftDeletingElement.java
+++ problems/arrays/LongestSubarrayOnesAftDeletingElement.java
@@ -1,5 +1,10 @@
 //1493. Longest Subarray of 1's After Deleting One Element
 
+// Intuition: 
+// Sliding Window: Track longest subarray with <= 1 zero
+// Expand right (count zeros), shrink left when count > 1
+// KEY: Return ans - 1 (the window size includes the element we need to delete)
+
 public class LongestSubarrayOnesAftDeletingElement {
 
     public int longestSubarray(int[] nums){
